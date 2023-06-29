dfd92dade8049fb5ed15a079d3a034f6ba9cea43
MarcoFSierra <136313327+MarcoFSierra@users.noreply.github.com>
2023-06-29 21:41:34 +0200

    RestauranteMalagaAlumnos
    restaurante

---
+++ restaurantemalagaalumno/src/main/java/edu/arelance/nube/RestaurantemalagaalumnoApplication.java
@@ -0,0 +1,13 @@
+package edu.arelance.nube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantemalagaalumnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantemalagaalumnoApplication.class, args);
+	}
+
+}
